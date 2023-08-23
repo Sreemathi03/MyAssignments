fb9dad936c2c7eeb05b235d59faaf029356d9f75
Sreemathi03 <141851536+Sreemathi03@users.noreply.github.com>
2023-08-23 18:46:18 +0530

    Add files via upload
    week3Day3

---
+++ Automation.java
@@ -0,0 +1,15 @@
+package week3.day3;
+
+public class Automation extends MultipleLanguage {
+
+	public static void main(String[] args) {
+
+		Automation obj = new Automation();
+		obj.python();
+		obj.ruby();
+		obj.selenium();
+		obj.java();
+
+	}
+
+}
---
+++ MultipleLanguage.java
@@ -0,0 +1,23 @@
+package week3.day3;
+
+public abstract class MultipleLanguage implements Language, TestTool {
+
+	public void python() {
+		System.out.println("python used in selenium automation");
+
+	}
+
+	public void ruby() {
+		System.out.println("Ruby used in Selenium automation");
+	}
+
+	public void java() {
+		System.out.println("java used in selenium automation");
+
+	}
+
+	public void selenium() {
+		System.out.println("Selenium supports all programming language");
+	}
+
+}
